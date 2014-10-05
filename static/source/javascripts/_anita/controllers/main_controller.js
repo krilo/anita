d2b0199d81eea8ee05b60b9484f58db06c220abe
Kristofer Forsell <kristofer@Kristofers-MacBook-Pro.local>
2014-10-05 15:17:35 +0200

    Added lock top and bottom

--- static/source/javascripts/_anita/controllers/main_controller.js
+++ static/source/javascripts/_anita/controllers/main_controller.js
@@ -7,7 +7,6 @@ function ($scope, $http, $rootScope) {
   $http({method: 'GET', url: 'data/anita.json'}).
     success(function(data, status, headers, config) {
       $scope.layers = data.layers;
-      $scope.container.height = window.innerHeight * 4
       $rootScope.$broadcast('landing:data-loaded');
     })
 
--- static/source/javascripts/_anita/directives/anita_scroll.js
+++ static/source/javascripts/_anita/directives/anita_scroll.js
@@ -7,24 +7,25 @@ anitaApp.directive("anitaScroll", [function() {
       var progress = 0;
       var resistance = attr.resistance || 100;
       var time = attr.time || 0.4;
+      var speed = attr.speed || 1.0;
     
       document.addEventListener('mousewheel', function(e){
 
         e.preventDefault();
         var delta = e.wheelDelta/120 || -e.detail/3;
-        progress = progress - delta/resistance //Math.min(Math.max(progress - delta/resistance, 0), 1)
+        progress = Math.min(Math.max(progress - delta/resistance, 0), 1)//progress - delta/resistance //Math.min(Math.max(progress - delta/resistance, 0), 1)
       
         updatePos()
       })
 
-      var updatePos = function(){
+      /*
+       * Tween Wrapper
+       */
 
-        /*
-         * Tween Wrapper
-         */
+      var updatePos = function(){
     
         TweenMax.to(container, time, {
-          y: Math.round((container.offsetHeight - window.innerHeight)*-progress),
+          y: Math.round(((container.offsetHeight*speed) - window.innerHeight)*-progress),
           x: Math.round(-container.offsetWidth/2),
           z: 1,
           ease: Power1.easeOut,
@@ -32,6 +33,15 @@ anitaApp.directive("anitaScroll", [function() {
         })
       }
 
+      /*
+       * Update Height
+       */
+
+      var updateHeight = function(){
+        scope.container.height = (scope.layer.items[scope.layer.items.length-1].position.y)
+      }
+
+      updateHeight()
       updatePos();
       
     }
